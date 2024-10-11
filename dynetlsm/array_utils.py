c54d72566f4971bc25ed624fb04f39694f1e3729
Adrian Röllin <adrian.roellin@nus.edu.sg>
2024-10-11 12:19:25 +0800

    Update array_utils.py
    np.bool to bool

--- dynetlsm/array_utils.py
+++ dynetlsm/array_utils.py
@@ -3,24 +3,24 @@ import numpy as np
 
 def triu_indices_from_3d(Y, k=0):
     return np.nonzero(~np.stack(
-        [np.tri(Y.shape[1], Y.shape[2], k=k-1, dtype=np.bool) for
+        [np.tri(Y.shape[1], Y.shape[2], k=k-1, dtype=bool) for
             t in range(Y.shape[0])]
     ))
 
 
 def diag_indices_from_3d(Y):
     return np.nonzero(np.stack(
-        [np.eye(Y.shape[1], Y.shape[2], dtype=np.bool) for
+        [np.eye(Y.shape[1], Y.shape[2], dtype=bool) for
             t in range(Y.shape[0])]
     ))
 
 
 def nondiag_indices_from_3d(Y):
     return np.nonzero(~np.stack(
-        [np.eye(Y.shape[1], Y.shape[2], dtype=np.bool) for
+        [np.eye(Y.shape[1], Y.shape[2], dtype=bool) for
             t in range(Y.shape[0])]
     ))
 
 
 def nondiag_indices_from(Y):
-    return np.nonzero(~np.eye(Y.shape[0], Y.shape[1], dtype=np.bool))
+    return np.nonzero(~np.eye(Y.shape[0], Y.shape[1], dtype=bool))
